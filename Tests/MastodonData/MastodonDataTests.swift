f9d7bb087d86c53d1efa21ba5c9361ef00243d68
Jonathan Pulfer <JonPulfer@users.noreply.github.com>
2024-09-21 20:06:49 +0100

    catch the require test error

--- Tests/MastodonData/MastodonDataTests.swift
+++ Tests/MastodonData/MastodonDataTests.swift
@@ -5,32 +5,34 @@
 //  Created by Jonathan Pulfer on 21/09/2024.
 //
 
-import Testing
-import MastodonData
 import Foundation
+import MastodonData
+import Testing
 
 @Suite struct TestDecoding {
-	let webFingerExample:String = """
- {
-
-   "subject": "acct:bugle@bugle.lol",
-   "links": [
+	let webFingerExample: String = """
 	 {
-	   "rel": "self",
-	   "type": "application/activity+json",
-	   "href": "https://bugle.lol/@bugle"
+
+	   "subject": "acct:bugle@bugle.lol",
+	   "links": [
+		 {
+		   "rel": "self",
+		   "type": "application/activity+json",
+		   "href": "https://bugle.lol/@bugle"
+		 }
+	   ]
 	 }
-   ]
- }
-"""
-	
-	@Test func TestDecodingWebFingerExample() throws {
+	"""
+
+	@Test func TestDecodingWebFingerExample() {
 		let jsonData = webFingerExample.data(using: .utf8)!
 		let decodedObject: WebFinger = try! JSONDecoder().decode(WebFinger.self, from: jsonData)
 		#expect(decodedObject.subject == "acct:bugle@bugle.lol")
-		#require(decodedObject.links.count == 1)
-		#expect(decodedObject.links[0].rel == "self")
-		#expect(decodedObject.links[0].type == "application/activity+json")
-		#expect(decodedObject.links[0].href == "https://bugle.lol/@bugle")
+		do {
+			try #require(decodedObject.links.count == 1)
+			#expect(decodedObject.links[0].rel == "self")
+			#expect(decodedObject.links[0].type == "application/activity+json")
+			#expect(decodedObject.links[0].href == "https://bugle.lol/@bugle")
+		} catch {}
 	}
 }

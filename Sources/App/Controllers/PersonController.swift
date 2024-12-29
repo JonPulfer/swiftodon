fcda51f7c9eccfa6177c03e2520669819b5087a8
Jonathan Pulfer <JonPulfer@users.noreply.github.com>
2024-12-29 13:56:24 +0000

    fix get person

--- Sources/App/Controllers/PersonController.swift
+++ Sources/App/Controllers/PersonController.swift
@@ -18,12 +18,10 @@ struct PersonController: RouterController {
 	var endpoints: RouteCollection<WebAuthnRequestContext> {
 		return RouteCollection(context: WebAuthnRequestContext.self)
 			.get(":id", use: get)
-			.post(use: create)
 	}
 	
 	var body: some RouterMiddleware<Context> {
-		Post(handler: self.create)
-		Get(handler: self.get)
+		Get("/:id", handler: self.get)
 	}
 
 	@Sendable func get(request: Request, context: some RequestContext) async throws -> Account? {
@@ -32,18 +30,5 @@ struct PersonController: RouterController {
 			return personObject.toMastodonAccount()
 		}
 		throw HTTPError(.notFound)
-	}
-
-	struct CreatePersonRequest: Decodable {
-		let handle: String
-		let fullName: String
-	}
-
-	@Sendable func create(request: Request, context: some RequestContext) async throws -> EditedResponse<Account?> {
-		let createRequest = try await request.decode(as: CreatePersonRequest.self, context: context)
-		if case let createdModel as Person = try await repository.create(from: CreatePerson(name: createRequest.handle, fullName: createRequest.fullName)) {
-			return EditedResponse(status: .created, response: createdModel.toMastodonAccount())
-		}
-		return EditedResponse(status: .badRequest, response: nil)
 	}
 }
